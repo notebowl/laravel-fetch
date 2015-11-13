2db8008fe54f9692209a72d387edaf2a3713b3ac
Alex Slaughter <alexs@notebowl.com>
2015-11-13 17:21:00 -0500

    add guzzle and get method

--- src/FetchHelper.php
+++ src/FetchHelper.php
@@ -2,8 +2,18 @@
 
 namespace NB\Utilities\Fetch;
 
+use GuzzleHttp\Client as HttpClient;
+
 class FetchHelper
 {
+    protected function get($url, $headers = [])
+    {
+        $client = new HttpClient();
+        $request = $client->get($url, $headers);
+
+        return $request->send();
+    }
+
     public function __call($method, $parameters)
     {
         return app('url')->$method(...$parameters);
