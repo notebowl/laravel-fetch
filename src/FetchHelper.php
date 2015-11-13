a1d07be06bfcc9b269656971e7badb5c9e6470a6
Alex Slaughter <alexs@notebowl.com>
2015-11-13 17:35:04 -0500

    add getBody and getResponseHeader methods

--- src/FetchHelper.php
+++ src/FetchHelper.php
@@ -6,6 +6,20 @@ use GuzzleHttp\Client as HttpClient;
 
 class FetchHelper
 {
+    public function getResponseHeader($url, $header, $headers = [])
+    {
+        $response = $this->get($url, $headers);
+
+        return $response->getHeader($header);
+    }
+
+    public function getBody($url, $headers = [])
+    {
+        $response = $this->get($url, $headers);
+
+        return $response->getBody();
+    }
+
     public function get($url, $headers = [])
     {
         $client = new HttpClient();
