13e1b3c0a39a98e221746c457745a9af39327899
Alex Slaughter <alexs@notebowl.com>
2015-11-21 21:41:53 -0500

    add retry function and guzzle library

--- src/FetchHelper.php
+++ src/FetchHelper.php
@@ -3,9 +3,31 @@
 namespace NB\Utilities\Fetch;
 
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Subscriber\Retry\RetrySubscriber as Retry;
 
 class FetchHelper
 {
+    public function getRetry($url, $headers = [], $tries = 3, $status = null, $delay = null)
+    {
+        $status = $status ?: [500, 503];
+        $retry = new Retry([
+            'filter' => Retry::createChainFilter([
+                Retry::createIdempotentFilter(),
+                Retry::createCurlFilter(),
+                Retry::createStatusFilter($status),
+            ]),
+            'max' => $tries,
+            'delay' => function ($retries) use ($delay) {
+                return $delay ?: (int) pow(2, $retries - 1);
+            },
+        ]);
+
+        $client = new HttpClient();
+        $client->getEmitter()->attach($retry);
+
+        return $client->get($url, $headers);
+    }
+
     public function getResponseHeader($url, $header, $headers = [])
     {
         $response = $this->get($url, $headers);
