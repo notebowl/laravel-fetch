6929657b9e7c125b6ba49adf7184149fd3d4397c
Alex Slaughter <alexs@notebowl.com>
2015-11-23 13:45:41 -0500

    add testing framework for travis

---
+++ tests/FetchHelperTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NB\Tests\Utilities\Fetch;
+
+use PHPUnit_Framework_TestCase;
+
+class FetchHelperTest extends PHPUnit_Framework_TestCase
+{
+    public function testGets()
+    {
+    }
+
+    public function testGetsRaw()
+    {
+    }
+
+    public function testGetsResponseHeader()
+    {
+    }
+
+    public function testGetsRetry()
+    {
+    }
+
+    public function testGetsRetryRaw()
+    {
+    }
+}
