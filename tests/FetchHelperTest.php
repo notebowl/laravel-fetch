50acd34f649afe648d9d7e3f0f99a875d1f4ddba
Alex Slaughter <alexs@notebowl.com>
2015-11-23 13:57:41 -0500

    add first test

--- tests/FetchHelperTest.php
+++ tests/FetchHelperTest.php
@@ -2,12 +2,17 @@
 
 namespace NB\Tests\Utilities\Fetch;
 
+use NB\Utilities\Fetch\FetchHelper;
 use PHPUnit_Framework_TestCase;
 
 class FetchHelperTest extends PHPUnit_Framework_TestCase
 {
     public function testGets()
     {
+        $f = new FetchHelper();
+
+        $body = '{"applicationName":"NoteBowl Campus Solution","productRelease":"1.0","versions":{"1.0":"https:\/\/api.notebowl.com\/v1.0"}}';
+        $this->assertEquals($f->get('https://api.notebowl.com'), $body);
     }
 
     public function testGetsRaw()
